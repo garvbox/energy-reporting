9828e7a17917b1846e71c61e218efcacaa6c9c34
garvinob <garv@garvbox.net>
2023-05-09 23:36:14 +0100

    Implement basic hard-coded rate calculations

--- scripts/influx_energy_reporting.py
+++ scripts/influx_energy_reporting.py
@@ -1,5 +1,6 @@
 #! /bin/env python3
 import sys
+from datetime import datetime
 from decimal import Decimal
 from typing import Optional
 
@@ -25,6 +26,36 @@ class Settings(BaseSettings):
         env_nested_delimiter = "_"
 
 
+# TODO: Store temp hard-coded rates elsewhere
+TIME_RATE_TYPES = {
+    0: "night",
+    1: "night",
+    2: "night",
+    3: "night",
+    4: "night",
+    5: "night",
+    6: "night",
+    7: "night",
+    8: "day",
+    9: "day",
+    10: "day",
+    11: "day",
+    12: "day",
+    13: "day",
+    14: "day",
+    15: "day",
+    16: "day",
+    17: "peak",
+    18: "peak",
+    19: "day",
+    20: "day",
+    21: "day",
+    22: "day",
+    23: "day",
+}
+RATE_TYPE_COSTS = {"day": Decimal("0.3095"), "night": Decimal("0.2008"), "peak": Decimal("0.3911")}
+
+
 def main():
     settings = Settings()  # type: ignore
     # getcontext().prec = settings.decimal_precision
@@ -43,20 +74,32 @@ def main():
     for (_, tag_data), values in data.items():
         device_name = tag_data["friendly_name"]
         total_energy = Decimal("0.00")
-        last_energy_val = Decimal("0.00")
+        total_cost = Decimal("0.00")
+        last_energy_val = None
         for time_max_data in values:
-            if not time_max_data["max"]:
+            if time_max_data["max"] is None:
                 # We are using linear interpolation above so this should only happen
                 #   when we have values missing from the start of a range. In that case there
                 #   is not much we can do with it so we have to just start later
                 continue
             cur_val = Decimal(time_max_data["max"])
-            if not last_energy_val:
+            if last_energy_val is None:
                 last_energy_val = cur_val
                 continue
             total_energy += cur_val - last_energy_val
+            cur_cost = get_rate(time_max_data["time"]) * (cur_val - last_energy_val)
+            total_cost += cur_cost
         total_energy = total_energy.quantize(Decimal("0.001"))
-        print(f"{device_name=} {total_energy}")
+        total_cost = total_cost.quantize(Decimal("0.00"))
+        print(f"{device_name=} {total_energy=} {total_cost=}")
+
+
+def get_rate(time_str: str) -> Decimal:
+    """Get rate from time in format:  2023-05-05T08:00:00Z"""
+    time = datetime.fromisoformat(time_str)
+    hour = time.hour
+    rate_type = TIME_RATE_TYPES[hour]
+    return RATE_TYPE_COSTS[rate_type]
 
 
 if __name__ == "__main__":
