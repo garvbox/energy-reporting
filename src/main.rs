7a74d7d2d677217191b5bea57a7786a39333f598
garvinob <garv@garvbox.net>
2023-08-19 13:43:54 +0100

    Add error logging and safe return from ping

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@ use axum::{response::Json, routing::get, Router};
 use influxdb::Client;
 use serde_json::{json, Value};
 use std::env;
+use tracing::{error, info};
+use tracing_subscriber;
 
 #[tokio::main]
 async fn main() {
     // build our application with a route
+    tracing_subscriber::fmt::init();
     let app = Router::new()
         // Ping DB test
         .route("/ping", get(ping_db));
@@ -17,11 +20,22 @@ async fn main() {
 }
 
 async fn ping_db() -> Json<Value> {
-    let influx_url = env::var("INFLUX_URL").unwrap();
+    info!("Pinging InfluxDB");
+    let influx_host = env::var("INFLUX_HOST").unwrap();
     let influx_db = env::var("INFLUX_DB").unwrap();
+    let influx_url = "http://".to_string() + &influx_host + ":8086";
     let client: Client = Client::new(influx_url, influx_db);
 
-    let ping_res = client.ping().await.unwrap();
+    let ping_res = match client.ping().await {
+        Ok(result) => {
+            info!("Ping OK");
+            result
+        }
+        Err(error) => {
+            error!("Ping Error {:?}", error);
+            return Json(json!({"error": error.to_string()}));
+        }
+    };
     let (server_type, res) = ping_res;
     // res
     Json(json!({ "server_type": server_type, "version": res }))
